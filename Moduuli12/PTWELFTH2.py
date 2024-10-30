b60a5bedd612a1b58d960c3348097b6edbc443ea
veikk <valttejo@metropolia.fi>
2024-10-30 09:44:21 +0200

    Git

--- Moduuli12/PTWELFTH2.py
+++ Moduuli12/PTWELFTH2.py
@@ -3,13 +3,13 @@ import json
 
 paikkakunta = str(input("Anna paikkakunnan nimi: "))
 
-pyyntö = f"http://api.openweathermap.org/geo/1.0/direct?q={paikkakunta}&limit={1}&appid=3801147f561ef583cf7466a41dae1dee"
+pyyntö = f"http://api.openweathermap.org/geo/1.0/direct?q={paikkakunta}&limit={1}&appid="
 vastaus = requests.get(pyyntö).json()
 
 lat = vastaus[0]["lat"]
 lon = vastaus[0]["lon"]
 
-pyyntö2 = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=3801147f561ef583cf7466a41dae1dee"
+pyyntö2 = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="
 vastaus2 = requests.get(pyyntö2).json()
 
 celsius = round(vastaus2["main"]["temp"] - 273.15, 0)
